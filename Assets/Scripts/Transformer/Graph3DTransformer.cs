e9b7741ca51bda10f530dec450b14449f0765bff
Thisisme-Andrew <thisemailstotroll@gmail.com>
2025-11-23 15:44:28 -0700

    randomization revmoal fix

--- Assets/Scripts/Transformer/Graph3DTransformer.cs
+++ Assets/Scripts/Transformer/Graph3DTransformer.cs
@@ -25,7 +25,6 @@ public class Graph3DTransformer : ITransformer
     private List<Vector3> GeneratePoints(float[] genes, int MIN_POINTS, int MAX_POINTS)
     {
         List<Vector3> points = new List<Vector3>();
-        var rand = new System.Random();
         
         // Map genes from [-1, 1] to usable parameters
         int nodeCount = MIN_POINTS + (int)(((genes[0] + 1f) / 2f) * (float)(MAX_POINTS - 3));  // 3-20 points
@@ -34,12 +33,15 @@ public class Graph3DTransformer : ITransformer
         // Generate random positions for each point (deterministically seeded by genes)
         for (int i = 0; i < nodeCount; i++)
         {
+            // Three random high prime numbers to help create hashes
+            float hash = ((i + 1) * 73856093f) % 1f;
+            float hash2 = ((i + 1) * 19349663f) % 1f;
+            float hash3 = ((i + 1) * 83492791f) % 1f;
+
             Vector3 pos = new Vector3(
-                // x, z = -0.5 to 0.5 * spreadFactor * 2f (for more spread)
-                // y = randomly incrementing higher 
-                ((float)rand.NextDouble() - 0.5f) * spreadFactor * 2f,
-                i * (float)rand.NextDouble(),
-                ((float)rand.NextDouble() - 0.5f) * spreadFactor * 2f
+                (hash - 0.5f) * spreadFactor * 2f,
+                i * 0.5f,
+                (hash2 - 0.5f) * spreadFactor * 2f
             );
             points.Add(pos);
         }
@@ -52,7 +54,6 @@ public class Graph3DTransformer : ITransformer
     private List<(int, int)> GenerateConnections(float[] genes, List<Vector3> points)
     {
         List<(int, int)> connections = new List<(int, int)>();
-        var rand = new System.Random();
 
         // Map genes from [-1, 1] to usable parameters
         float branchProbability = Mathf.Clamp01(genes[2] + 1f) / 2f; // branching likelihood
@@ -69,7 +70,8 @@ public class Graph3DTransformer : ITransformer
             // Probabilistically connect to other nodes based on genome seed (skip itself and next)
             for (int j = i + 2; j < points.Count; j++)
             {
-                float connectionHash = (float)rand.NextDouble();
+                // random high prime numbers to help create hash
+                float connectionHash = ((i * 1000 + j) * 73856093f) % 1f;
                 if (connectionHash < branchProbability)
                 {
                     connections.Add((i, j));
