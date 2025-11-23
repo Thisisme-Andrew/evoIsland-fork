0533a18d4a120c7c13cbdc8916131c9e61d705ea
Thisisme-Andrew <thisemailstotroll@gmail.com>
2025-11-23 15:34:16 -0700

    random generation of points and connections fixed

--- Assets/Scripts/Transformer/Graph3DTransformer.cs
+++ Assets/Scripts/Transformer/Graph3DTransformer.cs
@@ -5,30 +5,78 @@ public class Graph3DTransformer : ITransformer
 {
     public void Transform(Genome genome, GameObject gameObject)
     {
-        List<Vector3> points = new List<Vector3>();
-        List<(int, int)> edges = new List<(int, int)>();
+        int MIN_POINTS = 3;
+        int MAX_POINTS = 20;
+        float[] genes = genome.genes.ToArray();
+        List<Vector3> points;
+        List<(int, int)> connections;
 
-        float[] vector = genome.genes.ToArray();
-
-        if (vector.Length < 3)
+        if (genes.Length != 3)
         {
             Debug.LogWarning("Vector must have at least 3 elements");
             return;
         }
 
-        int numPoints = vector.Length / 3;
+        points = GeneratePoints(genes, MIN_POINTS, MAX_POINTS);
+        connections = GenerateConnections(genes, points);
+    }
+    
+    // Uses gene[0] to set number of points, use gene[1] to set the spreadfactor of each point generated
+    private List<Vector3> GeneratePoints(float[] genes, int MIN_POINTS, int MAX_POINTS)
+    {
+        List<Vector3> points = new List<Vector3>();
+        var rand = new System.Random();
+        
+        // Map genes from [-1, 1] to usable parameters
+        int nodeCount = MIN_POINTS + (int)(((genes[0] + 1f) / 2f) * (float)(MAX_POINTS - 3));  // 3-20 points
+        float spreadFactor = Mathf.Abs(genes[1]); // how spread out
 
-        for (int i = 0; i < numPoints; i++)
+        // Generate random positions for each point (deterministically seeded by genes)
+        for (int i = 0; i < nodeCount; i++)
         {
-            float x = vector[i * 3];
-            float y = vector[i * 3 + 1];
-            float z = vector[i * 3 + 2];
-            points.Add(new Vector3(x, y, z));
+            Vector3 pos = new Vector3(
+                // x, z = -0.5 to 0.5 * spreadFactor * 2f (for more spread)
+                // y = randomly incrementing higher 
+                ((float)rand.NextDouble() - 0.5f) * spreadFactor * 2f,
+                i * (float)rand.NextDouble(),
+                ((float)rand.NextDouble() - 0.5f) * spreadFactor * 2f
+            );
+            points.Add(pos);
         }
 
-        for (int i = 0; i < points.Count - 1; i++)
+        return points;
+    }
+
+
+    // Uses gene[2] to set branching probablity from each point
+    private List<(int, int)> GenerateConnections(float[] genes, List<Vector3> points)
+    {
+        List<(int, int)> connections = new List<(int, int)>();
+        var rand = new System.Random();
+
+        // Map genes from [-1, 1] to usable parameters
+        float branchProbability = Mathf.Clamp01(genes[2] + 1f) / 2f; // branching likelihood
+
+        // Connect points based on genome-seeded probability
+        for (int i = 0; i < points.Count; i++)
         {
-            edges.Add((i, i + 1));
+            // Always connect to next node if it exists (linear chain)
+            if (i < points.Count - 1)
+            {
+                connections.Add((i, i + 1));
+            }
+
+            // Probabilistically connect to other nodes based on genome seed (skip itself and next)
+            for (int j = i + 2; j < points.Count; j++)
+            {
+                float connectionHash = (float)rand.NextDouble();
+                if (connectionHash < branchProbability)
+                {
+                    connections.Add((i, j));
+                }
+            }
         }
+
+        return connections;
     }
 }
